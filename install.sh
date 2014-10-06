05cfb604b5482dabb0536866527d788992d7dfc7
Connor Peck <cpeck1@ualberta.ca>
2014-10-06 16:38:44 -0600

    Added prompt for container number to install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-get install libusb-1.0-0-dev
 apt-get install libftdi-dev
 apt-get install libcurl4-openssl-dev
@@ -11,3 +13,10 @@ mv /etc/udev/rules.d/99-input.rules /etc/udev/rules.d/97-input.rules
 
 cp client/pi/98-usb-probe.rules /etc/udev/rules.d/
 
+cnum=""
+while [[ ! $cnum =~ ^[0-9]+$ ]]; do
+    echo Enter container number
+    read cnum
+done
+
+sed -i "s/container_num: [0-9]*/container_num: $cnum/g" client/globals.ini
